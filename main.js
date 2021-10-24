d855a5e9bc1389a62418f8b8b6db5e071a35a673
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-10-24 07:12:22 +0530

    Add row match checking function

--- main.js
+++ main.js
@@ -11,38 +11,114 @@ const COLOURS = [
 ];
 
 const gameState = {
-    gameBoard: []
+    gameBoard: [],
+    checkMatchChanges: false
 };
 
 function main() {
+
     createBoard();
+    drawBoard();
 
     console.log(gameState.gameBoard);
+
+    checkMatches();
 }
 
 main();
 
 function createBoard() {
+
     gameState.gameBoard = [];
 
     for (let i = 0; i < BOARD_SIZE; i++) {
         const row = [];
 
         for (let j = 0; j < BOARD_SIZE; j++) {
-            row.push(`${i},${j}`);
+            const randomColour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
+            row.push(randomColour);
         }
 
         gameState.gameBoard.push(row);
     }
+}
+
+function drawBoard() {
 
     gameState.gameBoard.forEach((row, i) => {
         row.forEach((value, j) => {
             const div = document.createElement('div');
             div.setAttribute('draggable', true);
+            div.setAttribute('data-id', `square:${i},${j}`);
             div.classList.add('square');
-            div.style.background = COLOURS[Math.floor(Math.random() * COLOURS.length)];
+            div.style.background = value;
             div.innerText = value;
             gameBoardElement.appendChild(div);
         });
     });
+}
+
+function checkMatches(minMatchCount = 3) {
+
+    console.log('checking rows');
+
+    const rowMatches = getRowMatches();
+
+    console.log(rowMatches);
+}
+
+function getRowMatches(minMatchCount = 3) {
+
+    const rowMatches = [];
+
+    rowLoop: for (let r = 0; r < gameState.gameBoard.length; r++) {
+        const row = gameState.gameBoard[r];
+
+        let matchEnd = 0;
+
+        colLoop: for (let c = 0; c < row.length; c++) {
+            // check if current column was already checked by previous matching
+            // skip until last column matched if found
+            // this will avoid matching smaller numbers matches if higher number match was found
+            if (c < matchEnd) {
+                continue;
+            }
+
+            const matchStart = c;
+            let matchCount = 1;
+
+            // iterate through 
+            matchCountLoop: while (row[matchStart] == row[matchStart + matchCount]) {
+                matchCount++;
+            }
+
+            // subtract 1 to get index of match end
+            matchEnd = matchStart + matchCount - 1;
+
+            if (matchCount >= minMatchCount) {
+                const matchDetails = {
+                    row: r,
+                    colStart: matchStart,
+                    colEnd: matchEnd,
+                    matchCount
+                };
+                rowMatches.push(matchDetails);
+            }
+
+            const currentColour = row[c];
+            const nextEle = row[c+1];
+            const nextNextEle = row[c+2];
+
+            if (currentColour == nextEle && currentColour == nextNextEle) {
+                const ele = gameBoardElement.querySelector(`[data-id='square:${r},${c}']`);
+                ele.innerText = 'match';
+                const ele1 = gameBoardElement.querySelector(`[data-id='square:${r},${c+1}']`);
+                ele1.innerText = 'match1';
+                const ele2 = gameBoardElement.querySelector(`[data-id='square:${r},${c+2}']`);
+                ele2.innerText = 'match2';
+            }
+        }
+    }
+
+    return rowMatches;
 }
